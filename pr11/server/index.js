baff944ca74b7dc1f2d3d3a21868c6ea425e1cc0
ThereDrD <drdtheuser@gmail.com>
2026-03-23 09:49:58 +0300

    fix server in pr11

--- pr11/server/index.js
+++ pr11/server/index.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = 3000;
+const allowedOrigins = new Set(["http://localhost:3001", "http://127.0.0.1:3001"]);
 
 const accessSecret = "access_secret";
 const refreshSecret = "refresh_secret";
@@ -27,6 +28,24 @@ const users = [
 const products = [];
 const refreshTokens = new Set();
 const allowedRoles = ["user", "seller", "admin"];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (origin && allowedOrigins.has(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-refresh-token, Accept");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
